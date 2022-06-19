9a507d510de9e4096f1d803ae64709ec24099273
diegofdg <diegofdg@gmail.com>
2022-06-19 08:00:37 -0300

    clase_04-trabajando_con_pool_de_conexiones

--- control-de-stock/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ control-de-stock/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,15 +1,26 @@
 package com.alura.jdbc.factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.sql.DataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 
 public class ConnectionFactory {
 	
+	private DataSource dataSource;
+	
+	public ConnectionFactory() {
+		var comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("");
+        comboPooledDataSource.setMaxPoolSize(10);
+        
+        this.dataSource = comboPooledDataSource;
+	}
+	
 	public Connection recuperaConexion() throws SQLException {
-		return DriverManager.getConnection(
-        		"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-        		"root",
-        		"");
+		return this.dataSource.getConnection();
 	}
 }
